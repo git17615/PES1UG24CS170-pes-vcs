dd24502dd2b19f8f09293205827fa0800dd3e48f
git17615 <gitanjaliajay16@gmail.com>
2026-04-20 10:35:21 +0530

    Implement buffer allocation and hash computation
    Allocate a buffer for object data and compute its hash. Handle existing objects by freeing the buffer.

--- object.c
+++ object.c
@@ -100,6 +100,26 @@ int object_write(ObjectType type, const void *data, size_t len, ObjectID *id_out
 
     char header[64];
     int header_len = snprintf(header, sizeof(header), "%s %zu", type_str, len) + 1;
+
+    
+    size_t total_len = header_len + len;
+    char *buffer = malloc(total_len);
+    if (!buffer) return -1;
+
+    memcpy(buffer, header, header_len);
+    memcpy(buffer + header_len, data, len);
+
+    compute_hash(buffer, total_len, id_out);
+
+    if (object_exists(id_out)) {
+        free(buffer);
+        return 0;
+    }
+
+    char path[512];
+    object_path(id_out, path, sizeof(path));
+
+    
     
     (void)type; (void)data; (void)len; (void)id_out;
     return -1;
